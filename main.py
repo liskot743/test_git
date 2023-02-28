71e589243e88188d05a74b884204bb570c3f93ce
Дмитрий <125550307+liskot743@users.noreply.github.com>
2023-02-28 10:44:39 +0000

    save time

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-print("hello world")
-def example(x, y):
-    return x * y
+import time
+# x = [1,2,4,6,10]
+# for i in x:
+#     time.sleep(5)
+#     print(i*i)
 
-print(example(6, 6))
+sec = time.time()
+struct = time.localtime(sec)
+x=time.strftime('%d.%m.%Y %H:%M', struct)
+print(x)
